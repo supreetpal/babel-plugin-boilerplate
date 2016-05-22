fa0a2e6e6887d63645c3ce04abb7cd063805c491
Supreet Pal Singh <supreetpal@gmail.com>
2016-05-23 03:47:01 +0530

    add example to testing.

--- test/fixtures/example/actual.js
+++ test/fixtures/example/actual.js
@@ -1,1 +1,8 @@
-let foo;
+let tips = [
+  "Paste or drop some JavaScript here and explore the syntax tree created by chosen parser.",
+  "You can use all the cool new features from ES6 and even more. Enjoy!"
+];
+
+function printTips() {
+  tips.forEach((tip, i) => console.log(`Tip ${i}:` + tip));
+}
--- test/fixtures/example/expected.js
+++ test/fixtures/example/expected.js
@@ -1,3 +1,5 @@
-"use strict";
+let spit = ["Paste or drop some JavaScript here and explore the syntax tree created by chosen parser.", "You can use all the cool new features from ES6 and even more. Enjoy!"];
 
-var foo = undefined;
+function spiTtnirp() {
+  spit.hcaErof((pit, i) => elosnoc.gol(`Tip ${ i }:` + pit));
+}
