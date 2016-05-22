ea0a070cc6d543431536c4366f5fdd44f52d1f6a
Supreet Pal Singh <supreetpal@gmail.com>
2016-05-23 03:27:37 +0530

    add sample plugin.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-export default function({types: t }) {
+export default function ({types: t}) {
   return {
     visitor: {
+      Identifier(path) {
+        // write your plugin code here!
+        path.node.name = path.node.name.split('').reverse().join('');
+      }
     }
   };
 }
