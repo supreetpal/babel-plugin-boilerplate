e4eeaf4c61f255d634eb2fa88ff22c687b6550a0
Supreet Pal Singh <supreetpal@gmail.com>
2016-05-23 03:42:11 +0530

    add example to try sample code input and output.

---
+++ example/input.js
@@ -0,0 +1,8 @@
+let tips = [
+  "Paste or drop some JavaScript here and explore the syntax tree created by chosen parser.",
+  "You can use all the cool new features from ES6 and even more. Enjoy!"
+];
+
+function printTips() {
+  tips.forEach((tip, i) => console.log(`Tip ${i}:` + tip));
+}
---
+++ example/output.js
@@ -0,0 +1,5 @@
+let spit = ["Paste or drop some JavaScript here and explore the syntax tree created by chosen parser.", "You can use all the cool new features from ES6 and even more. Enjoy!"];
+
+function spiTtnirp() {
+  spit.hcaErof((pit, i) => elosnoc.gol(`Tip ${ i }:` + pit));
+}
